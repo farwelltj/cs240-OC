328657cf03e99a04a8c3ee9ab34c5b7cd5f407f2
Ture Farwell <farwelltj1988@gmail.com>
2012-11-07 10:43:33 -0500

    scion character sheeet
    starting with basic character info. raw_info works in idle but not
    sublime

---
+++ Scion Character Sheet.py
@@ -0,0 +1,25 @@
+class hero:
+	name = raw_input('character name')
+
+
+
+#	pantheon = raw_input('insert pantheon here')
+#	calling = raw_input('insert calling here')
+#	nature =  raw_input('insert nature here')
+#	god = raw_input('insert god here')
+
+
+#class attributes:
+
+
+
+#class abilities:
+
+
+
+
+#class augmentation:
+
+
+
+#class combat:
